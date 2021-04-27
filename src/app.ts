3ab5b835b487a3cdae6170b73393954240c294df
greatSumini <greatsumini@gmail.com>
2021-04-27 09:47:39 +0900

    chore: add .env.sample

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,25 @@
 import fastify from 'fastify';
+import { config } from 'dotenv';
 
 import { fastifyCron } from './plugins';
 import { jobsRoute } from './routes';
 
-export const buildFastify = () => {
-  const instance = fastify({ logger: false });
+config();
 
-  instance.get('/', async () => {
+export const buildFastify = () => {
+  const app = fastify({ logger: false });
+
+  app.get('/', async () => {
     return 'Hello Cron!';
   });
 
-  instance.register(fastifyCron);
-  instance.register(jobsRoute);
+  app.register(fastifyCron);
+  app.register(jobsRoute);
 
-  return instance;
+  return app;
 };
 
-buildFastify().listen(8080, (err, address) => {
+buildFastify().listen(process.env.PORT || 8080, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
